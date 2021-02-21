aa5b9112bbaf59de57afb54a3aed300e7a00ec6f
duniahoster <duniahoster@gmail.com>
2021-02-21 22:55:13 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -11,6 +11,10 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore consequatur ab at doloribus suscipit dolore adipisci perferendis ad quibusdam soluta excepturi, placeat ea optio aliquid ipsam tempora! Sunt, quae odit.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 
 </html>
